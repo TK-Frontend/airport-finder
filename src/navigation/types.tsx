85873fbed4345f4225d68a08df789f30b6dc8b02
unknown <teqa@wp.pl>
2022-05-11 15:04:20 +0200

    Created type for ProfileScreenRouteProp

---
+++ src/navigation/types.tsx
@@ -0,0 +1,7 @@
+import type { RouteProp } from "@react-navigation/native";
+import { RootStackParamList } from ".";
+
+type ProfileScreenRouteProp = RouteProp<
+  RootStackParamList,
+  "AirportDetailsScreen"
+>;
